e344b94f34009c30a7fa0ac85c5a0b9837c8976b
14eyes <shaverj@protonmail.com>
2022-07-01 04:23:11 +0300

    Improve plugin structure and logging

--- Cpp2IL.Plugin.GI/GIPlugin.cs
+++ Cpp2IL.Plugin.GI/GIPlugin.cs
@@ -21,40 +21,55 @@ public class GIPlugin : Cpp2IlPlugin
     }
     public override bool HandleGamePath(string gamePath, ref Cpp2IlRuntimeArgs args)
     {
+        var start = DateTime.Now;
         var exeName = Path.GetFileNameWithoutExtension(Directory.GetFiles(gamePath)
             .FirstOrDefault(f => f.EndsWith(".exe") && !MiscUtils.BlacklistedExecutableFilenames.Any(f.EndsWith)));
 
         args.PathToAssembly = Path.Combine(gamePath, $"{exeName}_Data", "Native", "UserAssembly.dll");
-        var unityPlayerPath = Path.Combine(gamePath, $"{exeName}.exe");
-        args.PathToMetadata = Path.Combine(gamePath, $"{exeName}_Data", "Managed", "Metadata", "global-metadata.dat");
-
-        if (File.Exists(args.PathToMetadata))
+        if (!File.Exists(args.PathToAssembly))
         {
-            Logger.VerboseNewline($"Found GI global-metadata. Attempting to Decrypt...");
+            Logger.WarnNewline($"Could not find GI UserAssembly at {args.PathToAssembly}");
+            return false;
+        }
+
+        args.PathToMetadata = Path.Combine(gamePath, $"{exeName}_Data", "Managed", "Metadata", "global-metadata.dat");
+        if (!File.Exists(args.PathToMetadata))
+        {
+            Logger.WarnNewline($"Could not find GI global-metadata at {args.PathToMetadata}");
+            return false;
+        }
+
+        try
+        {
+            Logger.Verbose($"Found GI global-metadata. Attempting to Decrypt...");
+
             var metadataRawBytes = File.ReadAllBytes(args.PathToMetadata);
             var decryptedRaw = Decrypter.decrypt_global_metadata(metadataRawBytes, metadataRawBytes.Length);
+
+            Logger.VerboseNewline($"OK ({(DateTime.Now - start).TotalMilliseconds:F0} ms)");
+
+            Logger.Verbose($"Decrypted metadata. Saving to temporary file...");
+
             var tmpPath = GetTemporaryFilePath();
             File.WriteAllBytes(tmpPath, decryptedRaw);
             args.PathToMetadata = tmpPath;
+
+            Logger.VerboseNewline($"OK ({(DateTime.Now - start).TotalMilliseconds:F0} ms)");
         }
+        catch (Exception ex)
+        {
+            Logger.ErrorNewline($"Exception while decrypting global-metadata {ex}");
+            return false;
+        }
+
+        Logger.InfoNewline($"global-metadata Decrypted Successfully");
 
         var gameDataPath = Path.Combine(gamePath, $"{exeName}_Data");
-        var uv = UnityVersion.Parse("2019.4.21");//Cpp2IlApi.DetermineUnityVersion(unityPlayerPath, gameDataPath);
-        Logger.VerboseNewline($"First-attempt unity version detection gave: {uv}");
-
-        if (uv == default)
-        {
-            Logger.Warn("Could not determine unity version, probably due to not running on windows and not having any assets files to determine it from. Enter unity version, if known, in the format of (xxxx.x.x), else nothing to fail: ");
-            var userInputUv = Console.ReadLine()!.Trim();
-
-            if (!string.IsNullOrEmpty(userInputUv))
-                uv = UnityVersion.Parse(userInputUv);
-        }
-
+        var uv = UnityVersion.Parse("2019.4.21"); // Forces IL2Cpp Version to 24.5
         args.UnityVersion = uv;
 
-        Logger.InfoNewline($"Determined game's unity version to be {args.UnityVersion}");
-
+        Logger.InfoNewline($"Set unity version to {args.UnityVersion}");
+        Logger.InfoNewline($"GIPlugin completed in {(DateTime.Now - start).TotalMilliseconds:F0}ms");
         return args.Valid = true;
     }
 }
